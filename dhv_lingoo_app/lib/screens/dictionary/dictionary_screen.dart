4480bb611b3b6e2134ee5115bc9a3889114bd2c8
maitamgk <maitamit062005@gmail.com>
2026-03-03 16:18:50 +0700

    docs(flutter): add description to dictionary_screen.dart

--- dhv_lingoo_app/lib/screens/dictionary/dictionary_screen.dart
+++ dhv_lingoo_app/lib/screens/dictionary/dictionary_screen.dart
@@ -1,3 +1,4 @@
+// Dictionary screen - Free Dictionary API lookup with phonetics and TTS
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_tts/flutter_tts.dart';
