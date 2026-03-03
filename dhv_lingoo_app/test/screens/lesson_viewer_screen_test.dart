10e26751e27157763351d88a55bcb45aa557f187
maitamgk <maitamit062005@gmail.com>
2026-03-03 16:39:29 +0700

    test(flutter): add lesson_viewer_screen screen test skeleton

---
+++ dhv_lingoo_app/test/screens/lesson_viewer_screen_test.dart
@@ -0,0 +1,2 @@
+import 'package:flutter_test/flutter_test.dart';
+void main(){group('lesson_viewer_screen screen',(){test('builds without error',()=>expect(true,isTrue));test('shows correct title',()=>expect(true,isTrue));});}
