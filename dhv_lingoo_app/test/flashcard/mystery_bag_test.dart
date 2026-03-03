3e859c76bb49e87cf0847bb2ce99237c99ccd13d
maitamgk <maitamit062005@gmail.com>
2026-03-04 00:42:07 +0700

    test(flutter/flashcard): add MysteryBagWidget test skeleton

---
+++ dhv_lingoo_app/test/flashcard/mystery_bag_test.dart
@@ -0,0 +1,2 @@
+import 'package:flutter_test/flutter_test.dart';
+void main(){group('MysteryBagWidget',(){test('renders correctly',()=>expect(true,isTrue));test('triggers onOpen callback',()=>expect(true,isTrue));test('plays opening animation',()=>expect(true,isTrue));});}
