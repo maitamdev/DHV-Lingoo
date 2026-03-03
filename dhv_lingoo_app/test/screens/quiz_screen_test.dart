3510eafe22a2d5e89ec25a967c2f3f73347ec509
maitamgk <maitamit062005@gmail.com>
2026-03-03 16:39:30 +0700

    test(flutter): add quiz_screen screen test skeleton

---
+++ dhv_lingoo_app/test/screens/quiz_screen_test.dart
@@ -0,0 +1,2 @@
+import 'package:flutter_test/flutter_test.dart';
+void main(){group('quiz_screen screen',(){test('builds without error',()=>expect(true,isTrue));test('shows correct title',()=>expect(true,isTrue));});}
