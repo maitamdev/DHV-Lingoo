b5820c62f1e82d63907c008dcb94fd219bfb2d3d
maitamgk <maitamit062005@gmail.com>
2026-03-03 16:39:31 +0700

    test(flutter): add vocabulary_model test skeleton

---
+++ dhv_lingoo_app/test/models/vocabulary_model_test.dart
@@ -0,0 +1,2 @@
+import 'package:flutter_test/flutter_test.dart';
+void main(){group('vocabulary_model',(){test('fromMap creates valid instance',()=>expect(true,isTrue));test('handles null values',()=>expect(true,isTrue));});}
