ce14d8b7bf2de21968d40e778e08f999af0f18cc
maitamgk <maitamit062005@gmail.com>
2026-03-04 00:42:08 +0700

    test(flutter/flashcard): add completion card test skeleton

---
+++ dhv_lingoo_app/test/flashcard/completion_test.dart
@@ -0,0 +1,2 @@
+import 'package:flutter_test/flutter_test.dart';
+void main(){group('CompletionCardWidget',(){test('shows XP earned',()=>expect(true,isTrue));test('has gradient background',()=>expect(true,isTrue));});}
