c127cdc08f4f85ea45debac805d3b663f19f94a1
maitamgk <maitamit062005@gmail.com>
2026-03-04 00:40:00 +0700

    test(flutter/flashcard): add FlashcardModel test skeleton

---
+++ dhv_lingoo_app/test/flashcard/flashcard_model_test.dart
@@ -0,0 +1,2 @@
+import 'package:flutter_test/flutter_test.dart';
+void main(){group('FlashcardModel',(){test('fromMap creates valid model',()=>expect(true,isTrue));test('handles null fields',()=>expect(true,isTrue));});}
