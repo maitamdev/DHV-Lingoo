533b3046a647c8dfbafe7179d46cd5f06e03b329
maitamgk <maitamit062005@gmail.com>
2026-03-04 00:45:26 +0700

    feat(flashcard): add flashcard constants

---
+++ src/lib/flashcard-constants.ts
@@ -0,0 +1,6 @@
+export const FLASHCARD_STORAGE_PREFIX = 'fc_';
+export const FLASHCARD_MAX_PER_DAY = 5;
+export const FLASHCARD_RESET_HOUR = 0;
+export const RARITY_ORDER = ['legendary', 'epic', 'rare', 'uncommon', 'common'];
+export const ANIMATION_DURATIONS = { bagOpen: 700, cardReveal: 800, cardFlip: 600, confetti: 3000, particleBurst: 700 };
+export const SOUND_ENABLED_KEY = 'flashcard_sound_enabled';
