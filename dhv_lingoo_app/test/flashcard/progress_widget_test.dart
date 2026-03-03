fb1ff8485779f244f81352a5d4ee98fb02ac4049
maitamgk <maitamit062005@gmail.com>
2026-03-04 00:42:08 +0700

    test(flutter/flashcard): add progress widget test skeleton

---
+++ dhv_lingoo_app/test/flashcard/progress_widget_test.dart
@@ -0,0 +1,2 @@
+import 'package:flutter_test/flutter_test.dart';
+void main(){group('FlashcardProgressWidget',(){test('shows correct count',()=>expect(true,isTrue));test('fills bar correctly',()=>expect(true,isTrue));});}
