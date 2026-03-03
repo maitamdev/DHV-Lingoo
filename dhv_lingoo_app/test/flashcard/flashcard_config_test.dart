69f29b0e4f2b173c88df610544b7a030815e1c81
maitamgk <maitamit062005@gmail.com>
2026-03-04 00:40:03 +0700

    test(flutter/flashcard): add FlashcardConfig test skeleton

---
+++ dhv_lingoo_app/test/flashcard/flashcard_config_test.dart
@@ -0,0 +1,2 @@
+import 'package:flutter_test/flutter_test.dart';
+void main(){group('FlashcardConfig',(){test('has 5 bag gradients',()=>expect(true,isTrue));test('xpPerCard has all rarities',()=>expect(true,isTrue));});}
