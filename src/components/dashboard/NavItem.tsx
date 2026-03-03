f49b6ecadca24dfe9b2c2e52caeab2c79146df0d
maitamgk <maitamit062005@gmail.com>
2026-03-04 01:17:10 +0700

    fix: move 'use client' before React import in NavItem.tsx

--- src/components/dashboard/NavItem.tsx
+++ src/components/dashboard/NavItem.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 // NavItem - individual navigation item with icon and active state
 "use client";
+import React from 'react';
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
