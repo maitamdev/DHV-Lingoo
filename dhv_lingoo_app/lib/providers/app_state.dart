1592e661a303ab819131e0585a3e7d9e4e07ce65
maitamgk <maitamit062005@gmail.com>
2026-03-03 16:44:18 +0700

    feat(flutter): add AppState model

---
+++ dhv_lingoo_app/lib/providers/app_state.dart
@@ -0,0 +1,1 @@
+class AppState { String? userId; String? userLevel; int totalXP; int streak; bool isLoggedIn; AppState({this.userId, this.userLevel, this.totalXP = 0, this.streak = 0, this.isLoggedIn = false}); }
