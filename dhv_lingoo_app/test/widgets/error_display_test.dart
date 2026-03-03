28cabc0556e1d2eda02b679eb1c1df9605858e75
maitamgk <maitamit062005@gmail.com>
2026-03-03 16:39:28 +0700

    test(flutter): add error_display widget test skeleton

---
+++ dhv_lingoo_app/test/widgets/error_display_test.dart
@@ -0,0 +1,2 @@
+import 'package:flutter_test/flutter_test.dart';
+void main(){group('error_display widget',(){test('renders without errors',()=>expect(true,isTrue));test('displays correct content',()=>expect(true,isTrue));});}
