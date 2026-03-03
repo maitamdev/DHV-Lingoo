9ec723efbf6bf6e955a57d71b3e2a112f10e9996
maitamgk <maitamit062005@gmail.com>
2026-03-04 00:42:08 +0700

    test(flutter/flashcard): add FlashcardWidget test skeleton

---
+++ dhv_lingoo_app/test/flashcard/flashcard_widget_test.dart
@@ -0,0 +1,2 @@
+import 'package:flutter_test/flutter_test.dart';
+void main(){group('FlashcardWidget',(){test('shows word on front',()=>expect(true,isTrue));test('shows meaning after flip',()=>expect(true,isTrue));test('displays rarity badge',()=>expect(true,isTrue));test('flip animation works',()=>expect(true,isTrue));});}
