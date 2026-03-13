214f36a5aa606ab6c79cafbd0eb494c858724fcd
maitamgk <maitamit062005@gmail.com>
2026-03-13 23:14:50 +0700

    refactor(lib): document metadata

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,2 +1,3 @@
 import type{Metadata}from'next';
 export function createMetadata(title:string,desc:string):Metadata{return{title:title+' | DHV-Lingoo',description:desc,openGraph:{title,description:desc,siteName:'DHV-Lingoo'}}}
+// SEO page title gen
