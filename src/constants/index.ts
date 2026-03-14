fbd3fab551c91207799bcd8ee73bbd4a8944dc98
maitamgk <maitamit062005@gmail.com>
2026-03-15 01:38:46 +0700

    feat(constants): add barrel export for all constants

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+/**
+ * DHV-Lingoo Constants
+ * Barrel export for all application constants
+ */
+export { LEVELS, getLevelByXp, getLevelColor } from './levels';
+export { RARITY_COLORS, ACHIEVEMENT_CATEGORIES } from './achievements';
+export { PRACTICE_MODES, GRADE_TIERS, getGrade } from './practice';
+export { DASHBOARD_NAV } from './navigation';
+export { XP_REWARDS, STREAK_BONUSES, getStreakMultiplier } from './xp-rewards';
+export { THEME_COLORS, GRADIENTS, CHART_COLORS } from './themes';
