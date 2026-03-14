3bb5f62e96670515f4ee979be03aeecba671ef11
maitamgk <maitamit062005@gmail.com>
2026-03-15 01:25:10 +0700

    feat(ui): add barrel export for all UI components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,12 @@
+/**
+ * UI Components barrel export
+ */
+export { default as LoadingSpinner } from './LoadingSpinner';
+export { default as EmptyState } from './EmptyState';
+export { default as Badge } from './Badge';
+export { default as ProgressBar } from './ProgressBar';
+export { default as Tooltip } from './Tooltip';
+export { default as Avatar } from './Avatar';
+export { default as StatCard } from './StatCard';
+export { default as ConfirmDialog } from './ConfirmDialog';
+export { default as Skeleton, SkeletonText } from './Skeleton';
